def625667b5b812dcf909b5bfe39c0121c8e5ee7
Struct <vxstruct@gmail.com>
2019-04-04 22:09:38 +1100

    Add info query shortcuts and performance logging

--- src/bot_modules/info_embed.py
+++ src/bot_modules/info_embed.py
@@ -1,15 +1,49 @@
 import data
 import re
+import config
+import logging
 from fuzzywuzzy import fuzz
 from fuzzywuzzy import process
 from hook import Hook
+import time
+
+logger = logging.getLogger(__name__)
 
 client = None
+shortcuts = {}
 
 
 async def on_init(discord_client):
-    global client
+    global client, shortcuts
     client = discord_client
+
+    logger.info("Resolving query shortcuts")
+
+    shortcut_config = config.get_global_config()["query_shortcuts"]
+    entity_maps = {
+        "adventurer": data.Adventurer.adventurers,
+        "dragon": data.Dragon.dragons
+    }
+
+    for etype in entity_maps:
+        emap = entity_maps[etype]
+        for short in shortcut_config[etype]:
+            expanded = shortcut_config[etype][short]
+            try:
+                entity = emap[expanded]
+            except KeyError:
+                logger.warning("Shortcut \"{0}\" = \"{1}\" doesn't resolve to any {2}".format(
+                    short,
+                    expanded,
+                    etype
+                ))
+                continue
+
+            if short in shortcuts:
+                logger.warning("Shortcut {0} resolves multiple times".format(short))
+            shortcuts[short] = entity
+
+    logger.info("Query shortcuts resolved.")
 
     Hook.get("on_message").attach(get_info)
 
@@ -20,14 +54,21 @@ async def get_info(message):
         if len(matches) > 0:
             search_locations = [
                 data.Adventurer.adventurers,
-                data.Dragon.dragons
+                data.Dragon.dragons,
+                shortcuts
             ]
 
             if len(matches) > 3:
                 await client.send_message(message.channel, "Too many queries, only the first three will be shown.")
 
             for match in matches[:3]:
+                query_start_time = time.clock()
+                if len(match) > 50:
+                    await client.send_message(message.channel, "That's way too long, I'm not looking for that.")
+
                 search_term = match.lower()
+
+                # check shortcuts
 
                 best_match = (None, 0, "")  # (matching item, match percent, match string)
                 for loc in search_locations:
@@ -44,6 +85,8 @@ async def get_info(message):
                         await client.send_message(message.channel, "I'm not sure what \"{0}\" is, did you mean \"{1}\"?".format(search_term, best_match[2]))
                     else:
                         await client.send_message(message.channel, "I'm not sure what \"{0}\" is.".format(search_term))
+
+                logger.info("Info query for \"{0}\" took {1} seconds".format(search_term, time.clock() - query_start_time))
 
 
 Hook.get("on_init").attach(on_init)
