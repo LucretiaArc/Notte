e1620da7923e4235e7d54b171d5eb8f623eb8d35
Struct <vxstruct@gmail.com>
2020-01-03 14:08:57 +1100

    Add aliases for euden to fix renaming

--- src/bot_modules/playing_status.py
+++ src/bot_modules/playing_status.py
@@ -12,11 +12,11 @@ async def on_init(discord_client):
     global client, statuses
     client = discord_client
     statuses = list(zip(*(config.get_global("status"))))
-    change_status()
+    change_status("my favourites!")
 
 
-def change_status():
-    new_status = random.choices(statuses[0], weights=statuses[1])[0]
+def change_status(new_status=None):
+    new_status = new_status or random.choices(statuses[0], weights=statuses[1])[0]
     asyncio.ensure_future(client.change_presence(activity=discord.Game(new_status)))
     asyncio.get_event_loop().call_later(600, change_status)
 
